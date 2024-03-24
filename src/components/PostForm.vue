4ba201ecfde7e96656089ce4b5b877d0abdaa458
Celine510 <celinewu1010@gmail.com>
2024-03-24 19:45:05 +0800

    feat: 🎸 create edit post dialog and form

---
+++ src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-form @submit.prevent="handleSubmit" ref="formEl">
+    <v-row>
+      <v-col cols="3">
+        <v-switch label="Published" v-model="form.published"></v-switch>
+      </v-col>
+      <v-col cols="3">
+        <v-checkbox indeterminate label="Published" v-model="form.published"></v-checkbox>
+      </v-col>
+    </v-row>
+
+    <v-text-field v-model="form.title" label="Title"></v-text-field>
+    <v-combobox
+      v-model="form.tags"
+      :items="['News', 'Tutorial', 'Event']"
+      label="Tags"
+      multiple
+      chips
+    ></v-combobox>
+    <v-file-input
+      accept="image/png, image/jpeg, image/bmp"
+      placeholder="Upload a Feature Image"
+      prepend-icon="mdi-camera"
+      label="Feature Image" 
+      v-model="form.image"
+    ></v-file-input>
+    <v-textarea label="Post Body" v-model="form.body"></v-textarea>
+
+    <button ref="submitBtn" type="submit" class="d-none">Submit</button>
+  </v-form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({});
+const form = ref({
+  title: "",
+  tags: [],
+  published: null,
+  body: "",
+  image: [],
+  ...props.post,
+});
+
+function handleSubmit() {
+  console.log("submitting", form.value);
+}
+
+const submitBtn = ref();
+const submit = () => submitBtn.value.click();
+
+defineExpose({ submit });
+</script>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -9,8 +9,8 @@
       hide-details
       single-line
     ></v-text-field>
-    
-    <v-data-table 
+
+    <v-data-table
       :items="posts"
       :headers="[
         {
@@ -25,29 +25,62 @@
       item-value="title"
       v-model="selectedPosts"
       :search="searchPost"
-    ></v-data-table>
+    >
+      <!-- eslint-disable-next-line vue/valid-v-slot -->
+      <template #item.title="{ item }">
+        <v-dialog fullscreen>
+          <template v-slot:activator="{ props: activatorProps }">
+            <button v-bind="activatorProps">{{ item.title }}</button>
+          </template>
+
+          <template v-slot:default="{ isActive }">
+            <v-card title="Edit Post">
+              <v-card-text>
+                <PostForm ref="postForm" :post="item" />
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn text="Cancel" @click="isActive.value = false"></v-btn>
+
+                <v-btn
+                  color="blue"
+                  variant="flat"
+                  text="Save Post" 
+                  @click="$event => 
+                    postForm.submit()
+                  "
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import PostForm from "@/components/PostForm.vue";
 
 const posts = ref([
-  { title: 'Post 1', author: 'Alice' },
-  { title: 'Post 2', author: 'Bob' },
-  { title: 'Post 3', author: 'Charlie' },
-  { title: 'Post 4', author: 'David' },
-  { title: 'Post 5', author: 'Eve' },
-  { title: 'Post 6', author: 'Frank' },
-  { title: 'Post 7', author: 'Grace' },
-  { title: 'Post 8', author: 'Henry' },
-  { title: 'Post 9', author: 'Ivy' },
-  { title: 'Post 10', author: 'Jack' },
-  { title: 'Post 11', author: 'Katie' },
-  { title: 'Post 12', author: 'Liam' }
+  { title: "Post 1", author: "Alice" },
+  { title: "Post 2", author: "Bob" },
+  { title: "Post 3", author: "Charlie" },
+  { title: "Post 4", author: "David" },
+  { title: "Post 5", author: "Eve" },
+  { title: "Post 6", author: "Frank" },
+  { title: "Post 7", author: "Grace" },
+  { title: "Post 8", author: "Henry" },
+  { title: "Post 9", author: "Ivy" },
+  { title: "Post 10", author: "Jack" },
+  { title: "Post 11", author: "Katie" },
+  { title: "Post 12", author: "Liam" },
 ]);
 
 const selectedPosts = ref([]);
-const searchPost = ref('');
-
+const searchPost = ref("");
+const postForm = ref();
 </script>
