d83801a84761fb4f9f6fd40812cb117c037e8832
Celine510 <celinewu1010@gmail.com>
2024-03-26 17:53:05 +0800

    fix: 🐛 vuetify's code(plugins forder) back to original

--- src/main.js
+++ src/main.js
@@ -10,23 +10,11 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
-// Vue Router
-import router from "@/router";
-
-// Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+// Plugins
+import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
-app.use(vuetify);
-app.use(router);
+registerPlugins(app);
 
 app.mount('#app')
